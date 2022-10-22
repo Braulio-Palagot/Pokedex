dd39c2411b0991ee6f5efae7cdc9d35f5ec3e586
Braulio David Hernández Palagot <bdpalagot@gmail.com>
2022-10-22 15:11:43 -0500

    ADD Conexión a la PokeApi
    Cambios Realizados:
        1) Se realizó la conexión con la PokeAPI para mostrar el listado de pokémones.
        2) Se realizó la conexión con la PokeAPI para mostrar los detales de cada pokémon.

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PokeapiService} from '../services/pokeapi.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  pokemon: any;
+  public stats: {
+    hp: number;
+    attack: number;
+    defense: number;
+    speed: number;
+  };
 
-  ngOnInit() {
+  constructor(
+    private pokeapi: PokeapiService,
+    private route: ActivatedRoute
+  ) { }
+
+  async ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.pokemon = await this.pokeapi.findPokemon(id);
+    this.loadStats();
   }
 
+  private loadStats() {
+    this.stats = {
+      hp: this.pokemon.stats[0].base_stat,
+      attack: this.pokemon.stats[1].base_stat,
+      defense: this.pokemon.stats[2].base_stat,
+      speed: this.pokemon.stats[5].base_stat
+    };
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {PokeapiService} from '../services/pokeapi.service';
+import {IonInfiniteScroll} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  public pokemonList: any[];
+  private offset: number;
 
+  constructor(private pokeapi: PokeapiService) {}
+
+  ngOnInit() {
+    this.pokemonList = [];
+    this.offset = 0;
+    this.loadPokemons();
+  }
+
+  async loadPokemons(showMore = false, event?: any) {
+    if (showMore) {
+      this.offset += 25;
+    }
+
+    const data = await this.pokeapi.getPokemonList(this.offset);
+    this.pokemonList = [...this.pokemonList, ...data];
+
+    if (event) {
+      event.target.complete();
+    }
+  }
+
+  onChangeSearch = async (event) => {
+    const searchValue = event.detail.value.replace(/\s/g, '');
+
+    if (!searchValue) {
+      this.pokemonList = [];
+      this.offset = 0;
+      this.loadPokemons();
+      return;
+    }
+
+    await this.pokeapi.findPokemon(searchValue)
+      .then(data => this.pokemonList = [data])
+      .catch(() => this.pokemonList = []);
+  };
 }
